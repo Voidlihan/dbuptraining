ba0b9fc477e31eea1c17796d75aea31d29bc022c
e.alihangt@gmail.com <e.alihangt@gmail.com>
2019-10-28 21:36:37 +0600

    Add project files.

---
+++ DbUpTraining/Book.cs
@@ -0,0 +1,16 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DbUpTraining
+{
+    //[Table("Books")]
+    public class Book
+    {
+        //[Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ DbUpTraining/Program.cs
@@ -0,0 +1,48 @@
+﻿using Dapper;
+using Dapper.Contrib.Extensions;
+using DbUp;
+using System;
+using System.Data.SqlClient;
+using System.Reflection;
+
+namespace DbUpTraining
+{
+    class Program
+    {
+        private const string CONNECTION_STRING = "Server=A-305-03;Database=BookStore;Trusted_Connection=true;";
+        static void Main(string[] args)
+        {
+            EnsureDatabase.For.SqlDatabase(CONNECTION_STRING);
+            using(var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var upgrader =
+                    DeployChanges.To
+                        .SqlDatabase(CONNECTION_STRING)
+                        .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                        .LogToConsole()
+                        .Build();
+
+                var result = upgrader.PerformUpgrade();
+                if (!result.Successful)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(result.Error);
+                    Console.ResetColor();
+#if DEBUG
+                    Console.ReadLine();
+#endif
+                }
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Success!");
+                Console.ResetColor();
+                var book = new Book
+                {
+                    Name = "Abay",
+                    Price = 5000,
+                };
+                //connection.Insert(book);
+                connection.Execute("Insert into Books values(@Id, @Name, @Price);", book);
+            }
+        }
+    }
+}
